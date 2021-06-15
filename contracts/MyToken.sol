6c1d42c50cfc860f32b95c5da3908a455761c81f
ajaz.s <ajaz.s@zohocorp.com>
2021-06-16 01:06:45 +0530

    unit test added

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.6.0;
 
-import "../client/node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20 {
     constructor(uint256 initialSupply) ERC20("Do Your Own Research", "DYOR") {
